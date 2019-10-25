f9f123cd4e1a6c614bbcb5acbfaf8f9108ad20dc
Irakli Mchedlishvili <irakli.mchedlishvili@datopian.com>
2019-10-25 19:05:38 +0400

    use background jobs instead of celery to run archive

--- ckanext/archiver/lib.py
+++ ckanext/archiver/lib.py
@@ -4,9 +4,27 @@ import ckan.plugins as p
 
 from ckan.model.types import make_uuid
 from ckan.lib.celery_app import celery
+from ckan.lib import jobs as job
+
+from ckanext.archiver.tasks import _update_resource as test_update_resource
+from ckanext.archiver.tasks import _update_package as test_update_package
 
 log = logging.getLogger(__name__)
 
+
+class Test:
+    def __init__(self, a):
+        self.a = a
+
+    def error(self, message, whatever='', a=None, b=None, c=None, d=None, e=None):
+        if whatever:
+            pass
+        print(message)
+
+    def info(self, message, whatever='', a=None, b=None, c=None, d=None, e=None):
+        if whatever:
+            pass
+        print(message)
 
 def create_archiver_resource_task(resource, queue):
     from pylons import config
@@ -17,9 +35,7 @@ def create_archiver_resource_task(resource, queue):
         package = resource.package
     task_id = '%s/%s/%s' % (package.name, resource.id[:4], make_uuid()[:4])
     ckan_ini_filepath = os.path.abspath(config['__file__'])
-    celery.send_task('archiver.update_resource',
-                     args=[ckan_ini_filepath, resource.id, queue],
-                     task_id=task_id, queue=queue)
+    job.enqueue(test_update_resource, [package.id], {'queue': queue, 'log': Test('a')})
     log.debug('Archival of resource put into celery queue %s: %s/%s url=%r',
               queue, package.name, resource.id, resource.url)
 
@@ -28,9 +44,7 @@ def create_archiver_package_task(package, queue):
     from pylons import config
     task_id = '%s/%s' % (package.name, make_uuid()[:4])
     ckan_ini_filepath = os.path.abspath(config['__file__'])
-    celery.send_task('archiver.update_package',
-                     args=[ckan_ini_filepath, package.id, queue],
-                     task_id=task_id, queue=queue)
+    job.enqueue(test_update_package, [package.id], {'queue': queue, 'log': Test('a')})
     log.debug('Archival of package put into celery queue %s: %s',
               queue, package.name)
 
